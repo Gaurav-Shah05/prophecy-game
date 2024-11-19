07e065a9d099899439e3fd47e4dbd29038d2ac13
NeerjaKasture <npkastar@gmail.com>
2024-11-20 04:51:27 +0530

    added instructions and improved interface

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,17 +1,15 @@
-import React from "react";
-
-const Buttons = ({text, otherClass, onClick}) => {
+const Buttons = ({ text, otherClass = "", onClick }) => {
     return (
-        <div 
-            onClick={onClick}
-            className={`bg-gradient-to-r from-blue-500 to-blue-600 text-white py-2 md:py-3 
-            w-full md:w-36 rounded-xl hover:from-blue-600 hover:to-blue-700 cursor-pointer 
-            transition-all duration-300 transform hover:-translate-y-1 
-            shadow-lg hover:shadow-xl text-center ${otherClass}`}
-        >
-            {text}
-        </div>
+      <div
+        onClick={onClick}
+        className={`bg-gradient-to-r from-blue-500 to-blue-600 text-white py-2 px-4 
+        rounded-xl hover:from-blue-600 hover:to-blue-700 cursor-pointer 
+        transition-all duration-300 transform hover:-translate-y-1 
+        shadow-lg hover:shadow-xl text-center w-fit mx-auto ${otherClass}`}
+      >
+        {text}
+      </div>
     );
-}
-
-export default Buttons;
+  };
+  
+  export default Buttons;
